af5f1e0faa17b586eb4212978bf89a10c2d35c0d
fei.chen <fchen24@outlook.com>
2020-10-16 09:41:01 +0800

    Initial Commit

---
+++ DiceRollApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiceRollApp
+//
+//  Created by 陳飛 on 16/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
